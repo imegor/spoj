146d3bcd7798b3c472bf31f8bc12d5414d4e3db3
imegor <43791210+imegor@users.noreply.github.com>
2018-10-06 09:49:25 +0530

    Create init.cpp
    This is a frame cpp file used by me.

---
+++ init.cpp
@@ -0,0 +1,36 @@
+/*Created By IMEGOR (Karan Kanojia) Date : October 2018 :: 9:14 AM :: Windows Operating System*/
+#include <bits/stdc++.h>
+#define MAX 1111111
+#define lim 1e9+7
+#define ll long long 
+#define SQRT 1060
+#define sci(a) scanf("%d", &a)
+#define scl(a) scanf("%lld", &a)
+#define pfi(a) printf("%d\n", a)
+#define pfl(a) printf("%lld\n", a)
+#define pfcase(t) printf("Case #%d:\n",t)
+#define pf(d) printf("%s\n",d)
+#define mp make_pair
+#define fi first
+#define se second
+#define pb push_back
+#define vec vector<ll> 
+
+using namespace std ;
+
+ll fast(ll a, ll b, ll m){ll answer = 1; while(b > 0){if(b&1){answer = (answer%m * a%m)%m;} a = (a%m*a%m)%m; b=b>>1; } return answer; }
+
+
+int main(){
+
+	//File handling start 
+	#ifndef ONLINE_JUDGE
+	freopen("input.txt","r", stdin);
+	freopen("output.txt", "w", stdout);
+	#endif
+	//File Handing ends 
+
+	//Code starts from here
+	
+	
+}
